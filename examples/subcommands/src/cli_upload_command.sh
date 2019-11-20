5131ff9a74e8ec89296b4c548d985fd4572795e3
Danny Ben Shitrit <db@dannyben.com>
2019-11-20 16:04:26 +0000

    add examples, documentation and integration tests

---
+++ examples/subcommands/src/cli_upload_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/cli_upload_command.sh'"
+echo "# code for 'cli upload' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
---
+++ spec/bashly/integration/script_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+# This spec tests that the generated bash scripts are actually doing
+# what they are supposed to do, and running without errors.
+# It will run several tests on all the configuration files in the examples
+# folder
+
+describe 'generated bash scripts' do
+  examples = Dir["examples/*"].select { |f| File.directory? f }
+
+  examples.each do |example|
+    describe example do
+      it "works" do
+        output = "not executed"
+        Dir.chdir example do
+          output = `bash test.sh 2>&1`
+        end
+        expect(output).to match_fixture(example)
+      end
+    end
+  end
+
+end
