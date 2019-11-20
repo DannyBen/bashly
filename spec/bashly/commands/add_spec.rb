d61719cc013fb90c4a611d539eef1c521a095bb0
Danny Ben Shitrit <db@dannyben.com>
2019-11-20 22:57:11 +0000

    add specs for add command

---
+++ spec/bashly/commands/add_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Commands::Add do
+  let(:source_dir) { Settings.source_dir }
+  let(:target_dir) { Settings.target_dir }
+  subject { CLI.runner }
+
+  context "with --help" do
+    it "shows long usage" do
+      expect{ subject.run %w[add --help] }.to output_fixture('cli/add/help')
+    end
+  end
+
+  context "without arguments" do
+    it "shows long usage" do
+      expect{ subject.run %w[add] }.to output_fixture('cli/generate/usage')
+    end
+  end
+
+  context "with strings command" do
+    let(:strings_file) { "#{source_dir}/bashly-strings.yml" }
+
+    before do
+      reset_tmp_dir create_src: true
+    end
+
+    it "copies the strings configuration to the user space" do
+      expect { subject.run %w[add strings] }.to output_fixture('cli/add/strings')
+      expect(File).to exist(strings_file)
+    end
+
+    context "when the source directory does not exist" do
+      before do
+        reset_tmp_dir
+      end
+
+      it "raises an error" do
+        expect { subject.run %w[add strings] }.to raise_error(InitError, /does not exist/)
+      end
+    end
+
+    context "when the file exists" do
+      it "skips copying it" do
+        expect { subject.run %w[add strings] }.to output_fixture('cli/add/strings')
+        expect { subject.run %w[add strings] }.to output_fixture('cli/add/strings-exist')
+      end
+    end
+  end
+
+end
--- spec/spec_mixin.rb
+++ spec/spec_mixin.rb
@@ -1,6 +1,7 @@
 module SpecMixin
-  def reset_tmp_dir
+  def reset_tmp_dir(create_src: false)
     system 'rm -rf spec/tmp/*'
     system 'mkdir -p spec/tmp'
+    system 'mkdir -p spec/tmp/src' if create_src
   end
 end
