5efcc0c4416d945f7aa9c73805df064176adb84a
Danny Ben Shitrit <db@dannyben.com>
2019-11-20 19:41:41 +0000

    move all hard coded strings to config

--- lib/bashly/settings.rb
+++ lib/bashly/settings.rb
@@ -10,6 +10,37 @@ module Bashly
       def target_dir
         @target_dir ||= ENV['BASHLY_TARGET_DIR'] || '.'
       end
+
+      def strings
+        @strings ||= strings!
+      end
+
+    private
+
+      def strings!
+        defaults = YAML.load_file strings_config_path
+        defaults.merge project_strings
+      end
+
+      def project_strings
+        @project_strings ||= project_strings!
+      end
+
+      def project_strings!
+        if File.exist? project_strings_path
+          YAML.load_file project_strings_path
+        else
+          {}
+        end
+      end
+
+      def project_strings_path
+        @project_strings_path ||= "#{source_dir}/bashly-settings.yml"
+      end
+
+      def strings_config_path
+        File.expand_path "templates/strings.yml", __dir__
+      end
     end
   end
 end
