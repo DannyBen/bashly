8d5ced6d74445b884274af7342ed50ddf010a729
Danny Ben Shitrit <db@dannyben.com>
2019-11-21 08:25:34 +0000

    extract strings to its own class and booya

--- lib/bashly/settings.rb
+++ lib/bashly/settings.rb
@@ -1,7 +1,6 @@
 module Bashly
   class Settings
     class << self
-      include AssetHelper
       attr_writer :source_dir, :target_dir
 
       def source_dir
@@ -10,33 +9,6 @@ module Bashly
 
       def target_dir
         @target_dir ||= ENV['BASHLY_TARGET_DIR'] || '.'
-      end
-
-      def strings
-        @strings ||= strings!
-      end
-
-    private
-
-      def strings!
-        defaults = YAML.load_file asset("templates/strings.yml")
-        defaults.merge project_strings
-      end
-
-      def project_strings
-        @project_strings ||= project_strings!
-      end
-
-      def project_strings!
-        if File.exist? project_strings_path
-          YAML.load_file project_strings_path
-        else
-          {}
-        end
-      end
-
-      def project_strings_path
-        @project_strings_path ||= "#{source_dir}/bashly-strings.yml"
       end
     end
   end
---
+++ spec/bashly/message_strings_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe MessageStrings do
+  describe '[]' do
+    it "returns a default string for use in the generated bash" do
+      expect(subject[:usage]).to eq "Usage:"
+    end
+  end
+
+  context "when there is an override user configuration file" do
+    let(:config_template) { "examples/custom-strings/src/bashly-strings.yml" }
+    let(:userspace) { Settings.source_dir }
+
+    before do
+      reset_tmp_dir create_src: true
+      success = system "cp #{config_template} #{userspace}"
+      expect(success).to be true
+    end
+
+    it "returns values from the user config, falling back to defaults" do
+      expect(subject[:usage]).to eq "== Usage ==\n"      
+    end
+  end
+end
