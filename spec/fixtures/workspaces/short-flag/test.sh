54770dfee42b281a108f0dc8da7ae47d551f6c93
Danny Ben Shitrit <db@dannyben.com>
2019-11-21 16:28:00 +0000

    improve ci by avoiding the echo of the rm commands

--- spec/fixtures/workspaces/short-flag/test.sh
+++ spec/fixtures/workspaces/short-flag/test.sh
@@ -3,10 +3,10 @@
 # This fixture tests that short flags work properly
 # Reference issue: https://github.com/DannyBen/bashly/issues/16
 
-set -x
-
 rm -f ./src/*.sh
 rm -f ./rush
+
+set -x
 
 bashly generate
 
