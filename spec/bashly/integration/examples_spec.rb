af219c34471716628cb66cf9181c46ee50b80a40
Danny Ben Shitrit <db@dannyben.com>
2021-02-18 10:00:41 +0000

    increase approval leeway

--- spec/bashly/integration/examples_spec.rb
+++ spec/bashly/integration/examples_spec.rb
@@ -14,7 +14,7 @@ describe 'generated bash scripts' do
 
   test_cases = fixtures + examples
 
-  leeway = ENV['CI'] ? 10 : 0
+  leeway = ENV['CI'] ? 30 : 0
 
   test_cases.each do |example|
     approval_name = example.gsub "spec/fixtures/workspaces", "examples"
