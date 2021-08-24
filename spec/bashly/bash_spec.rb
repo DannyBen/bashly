24dfda9866470a79dbd3a7a66c26884798cfa1f3
Danny Ben Shitrit <db@dannyben.com>
2021-08-24 06:45:13 +0000

    attempt fix ci

--- spec/bashly/bash_spec.rb
+++ spec/bashly/bash_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'bash' do
   context "when bash version is < 4" do
+    before { system "docker pull bash:3 >/dev/null" }
+
     it "errors gracefully" do
       command = "docker run --rm -v $PWD:/app bash:3 bash /app/download"
 
