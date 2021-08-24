842020ca06ffc5c9307afa6c4742c702868908b1
Danny Ben Shitrit <db@dannyben.com>
2021-08-24 06:32:06 +0000

    attempt fix ci

--- spec/bashly/bash_spec.rb
+++ spec/bashly/bash_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'bash' do
   context "when bash version is < 4" do
     it "errors gracefully" do
-      command = "docker run --rm -it -v $PWD:/app bash:3 bash /app/download"
+      command = "docker run --rm -v $PWD:/app bash:3 bash /app/download"
 
       Dir.chdir "examples/minimal" do
         expect(`#{command} 2>&1`).to match_approval('bash/error')
