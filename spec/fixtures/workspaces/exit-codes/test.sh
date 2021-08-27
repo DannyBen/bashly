0d1c3db31ae9073a38684e348f8b629e38ead3d7
Danny Ben Shitrit <db@dannyben.com>
2021-08-27 09:16:53 +0000

    update

---
+++ spec/fixtures/workspaces/exit-codes/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This fixture tests that short command codes work properly
+# It is executed as part of the Runfile examples test
+# Reference issue: https://github.com/DannyBen/bashly/issues/16
+
+rm -f ./src/*.sh
+rm -f ./rush
+
+set -x
+
+bashly generate
+
+./rush c
+./rush g
