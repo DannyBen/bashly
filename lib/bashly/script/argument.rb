ce85408a06dcdc9e3ff4f75175a0e40609e06365
Danny Ben Shitrit <db@dannyben.com>
2021-11-14 18:22:56 +0000

    - Add basic validation for key config properties

--- lib/bashly/script/argument.rb
+++ lib/bashly/script/argument.rb
@@ -4,6 +4,10 @@ module Bashly
       def usage_string
         required ? name.upcase : "[#{name.upcase}]"
       end
+
+      def verify
+        raise ConfigurationError, "Argument must have a name" unless name
+      end
     end
   end
 end
