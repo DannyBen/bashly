9bda55cfda637d5abde1a354dd0e26f5cd724508
Danny Ben Shitrit <db@dannyben.com>
2021-11-21 18:21:57 +0000

    - Refactor catch_all (internal)

---
+++ lib/bashly/script/catch_all.rb
@@ -0,0 +1,49 @@
+module Bashly
+  module Script
+    class CatchAll
+      class << self
+        def from_config(config)
+          options = case config
+          when nil
+            { enabled: false }
+          when String
+            { label: config }
+          when Hash
+            { label: config['label'], help: config['help'], required: config['required'] }
+          else
+            {}
+          end
+
+          new **options
+        end
+      end
+
+      def initialize(label: nil, help: nil, required: false, enabled: true)
+        @label, @help, @required, @enabled = label, help, required, enabled
+      end
+
+      def enabled?
+        @enabled
+      end
+
+      def label
+        enabled? ? "#{@label&.upcase}..." : nil
+      end
+
+      def help
+        enabled? ? @help : nil
+      end
+
+      def required?
+        @required
+      end
+
+      def usage_string
+        return nil unless enabled?
+        required? ? label : "[#{label}]"
+      end
+
+    end
+  end
+end
+
