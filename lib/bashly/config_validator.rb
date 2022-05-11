174cc94666f006460e6e5cff7398dd0ffcc92898
Danny Ben Shitrit <db@dannyben.com>
2022-05-11 17:15:34 +0000

    extract basic assertions from ConfigValidator to ValidationHelpers

--- lib/bashly/config_validator.rb
+++ lib/bashly/config_validator.rb
@@ -1,5 +1,7 @@
 module Bashly
   class ConfigValidator
+    include ValidationHelpers
+
     attr_reader :data
 
     def initialize(data)
@@ -10,68 +12,7 @@ module Bashly
       assert_command "root", data
     end
 
-    def deprecations
-      @deprecations ||= []
-    end
-
   private
-
-    def assert(valid, message)
-      raise ConfigurationError, message unless valid
-    end
-
-    def refute(invalid, message)
-      assert !invalid, message
-    end
-
-    def deprecate(key, **options)
-      deprecations.push Deprecation.new(key, **options)
-    end
-
-    def assert_string(key, value)
-      assert value.is_a?(String), "#{key} must be a string"
-    end
-
-    def assert_optional_string(key, value)
-      assert_string key, value if value
-    end
-
-    def assert_boolean(key, value)
-      assert [true, false, nil].include?(value), "#{key} must be a boolean" 
-    end
-
-    def assert_array(key, value, of: nil)
-      return unless value
-      assert value.is_a?(Array), "#{key} must be an array"
-      if of
-        value.each_with_index do |val, i|
-          send "assert_#{of}".to_sym, "#{key}[#{i}]", val
-        end
-      end
-    end
-
-    def assert_hash(key, value, whitelist = nil)
-      assert value.is_a?(Hash), "#{key} must be a hash"
-      
-      if whitelist
-        invalid_keys = value.keys.map(&:to_sym) - whitelist
-        assert invalid_keys.empty?, "#{key} contains invalid options: #{invalid_keys.join(', ')}"
-      end
-    end
-
-    def assert_uniq(key, value, array_key)
-      return unless value
-      list = value.map { |c| c[array_key] }.compact.flatten
-      assert list.uniq?, "#{key} cannot have elements with similar #{array_key} values"
-    end
-
-    def assert_string_or_array(key, value)
-      return unless value
-      assert [Array, String].include?(value.class),
-        "#{key} must be a string or an array"
-
-      assert_array key, value, of: :string if value.is_a? Array
-    end
 
     def assert_version(key, value)
       return unless value
