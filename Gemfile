330945b2ceab9c532f6ce1e3a70b45a5fff28897
Danny Ben Shitrit <db@dannyben.com>
2022-06-13 15:14:35 +0000

    - Switch from ERB to GTX templates

--- Gemfile
+++ Gemfile
@@ -7,5 +7,6 @@ gem 'rspec_approvals'
 gem 'runfile'
 gem 'runfile-tasks'
 gem 'simplecov'
+gem 'gtx', path: '/vagrant/gems/gtx'
 
 gemspec
--- lib/bashly/concerns/renderable.rb
+++ lib/bashly/concerns/renderable.rb
@@ -1,12 +1,17 @@
 require 'erb'
+require 'gtx'
 
 module Bashly
   module Renderable
     def render(view)
-      template = File.read view_path(view)
-      erb = ERB.new(template, trim_mode: '%-')
-      erb.filename = "#{views_subfolder}.#{view}"
-      erb.result binding
+      if erb? view
+        template = File.read erb_view_path(view)
+        erb = ERB.new(template, trim_mode: '%-')
+        erb.filename = "#{views_subfolder}.#{view}"
+        erb.result binding
+      else
+        GTX.render_file view_path(view), context: binding, filename: "#{views_subfolder}.#{view}"
+      end
     end
 
     def strings
@@ -36,7 +41,15 @@ module Bashly
 
   private
 
+    def erb?(view)
+      File.exist? erb_view_path view
+    end
+
     def view_path(view)
+      "#{self_views_path}/#{view}.gtx"
+    end
+
+    def erb_view_path(view)
       "#{self_views_path}/#{view}.erb"
     end
 
