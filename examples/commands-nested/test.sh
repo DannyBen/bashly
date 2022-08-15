fa623e6d5310c9d0493830aa0297b25f35829543
Danny Ben Shitrit <db@dannyben.com>
2022-08-15 17:04:31 +0000

    - Show error instead of usage on invalid command call

--- examples/commands-nested/test.sh
+++ examples/commands-nested/test.sh
@@ -12,10 +12,14 @@ bashly generate
 ./cli -h
 ./cli dir
 ./cli file
+# verify invalid commands properly error
+./cli dig
 ./cli dir -h
 ./cli file -h
 ./cli dir list
 ./cli dir list -h
+# verify invalid subcommands properly error
+./cli dir lost -h
 ./cli file edit
 ./cli file edit -h
 ./cli file edit filename
