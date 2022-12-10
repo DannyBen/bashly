ac7b053e24daafa61767f214d36b90aff01e0ed3
Danny Ben Shitrit <db@dannyben.com>
2022-12-10 18:47:04 +0000

    add repeatable arg validations regression test

---
+++ spec/fixtures/workspaces/repeatable-arg-validations/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+rm -f ./cli
+rm -f ./src/initialize.sh
+rm -f ./src/root_command.sh
+
+set -x
+
+bundle exec bashly generate
+
+./cli
+./cli 1
+./cli 1 2
+./cli 1.1
+./cli 1.1 2
+./cli 1 2.2
