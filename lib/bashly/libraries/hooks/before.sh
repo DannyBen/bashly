c0ce61084db9fc7d0bba6827b3e243c1a4fcffd4
Danny Ben Shitrit <db@dannyben.com>
2023-03-21 09:49:42 +0000

    - Add 'bashly add hooks' command

---
+++ lib/bashly/libraries/hooks/before.sh
@@ -0,0 +1,7 @@
+## before hook
+##
+## Any code here will be placed inside a `before_hook()` function and called
+## before running any command (but after processing its arguments).
+##
+## You can safely delete this file if you do not need it.
+inspect_args
