3e4cd6d04b393292bd6124ef2968aaa8fed04e29
Danny Ben Shitrit <db@dannyben.com>
2023-04-08 06:52:08 +0000

    remove redundant comment

--- spec/fixtures/workspaces/short-flag/test.sh
+++ spec/fixtures/workspaces/short-flag/test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# This fixture tests that alias command codes work properly
-# It is executed as part of the Runfile examples test
-# Reference issue: https://github.com/DannyBen/bashly/issues/16
-
 rm -f ./src/*.sh
 rm -f ./cli
 
