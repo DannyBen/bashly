c5689a59fbabf5e38fb4038cc42c143ca925bb9a
Danny Ben Shitrit <db@dannyben.com>
2023-08-05 06:05:05 +0000

    improve config library

--- examples/config-ini/src/lib/config.sh
+++ examples/config-ini/src/lib/config.sh
@@ -2,20 +2,46 @@
 ## This file is a part of Bashly standard library
 ##
 ## Usage:
+##
 ## - In your script, call `config_load path/to/config.ini`.
 ## - A global associative array named `config` will become available to you,
-##   and can be accessed like `${config[section1.key1]}`
+## - When updating any of the associative array's values, call
+##   `config_save path/to/config.ini` to save the data.
+## - If a global variable named CONFIG_FILE exists, you can omit the filename
+##   in `config_load` and `config_save`.
+## - INI sections are optional.
 ##
+## Get a value:
+##
+##   ${config[section1.key1]}  # for keys under a [section]
+##   ${config[key1]}           # for keys not under a [section]
+##   ${config[key1]:-default}  # get a default value if the config key is unset
+##
+## Update/create a value:
+##
+##   config[section1.key1]="value"
+##   config_save path/to/config.ini
+##
+## Delete a value
+##
+##   unset config[section1.key1]
+##   config_save path/to/config.ini
+
+## Load a config file and populate the associative array `config`.
+# shellcheck disable=SC2120
 config_load() {
   declare -gA config
 
-  local config_file="$1"
+  local config_file="${1:-$CONFIG_FILE}"
+
   local section=""
   local key=""
   local value=""
   local section_regex="^\[(.+)\]"
   local key_regex="^([^ =]+) *= *(.*) *$"
   local comment_regex="^;"
+
+  [[ -f "$config_file" ]] || touch "$config_file"
 
   while IFS= read -r line; do
     if [[ $line =~ $comment_regex ]]; then
@@ -30,22 +56,24 @@ config_load() {
   done <"$config_file"
 }
 
-## Save the array back to a file
+## Save the associative array `config` back to a file
+# shellcheck disable=SC2120
 config_save() {
-  local filename="$1"
+  local config_file="${1:-$CONFIG_FILE}"
+
   local current_section=""
   local has_free_keys=false
 
-  rm -f "$filename"
+  rm -f "$config_file"
 
   for key in $(config_keys); do
     [[ $key == *.* ]] && continue
     has_free_keys=true
     value="${config[$key]}"
-    echo "$key = $value" >>"$filename"
+    echo "$key = $value" >>"$config_file"
   done
 
-  [[ "${has_free_keys}" == "true" ]] && echo >>"$filename"
+  [[ "${has_free_keys}" == "true" ]] && echo >>"$config_file"
 
   for key in $(config_keys); do
     [[ $key == *.* ]] || continue
@@ -53,12 +81,12 @@ config_save() {
     IFS="." read -r section_name key_name <<<"$key"
 
     if [[ "$current_section" != "$section_name" ]]; then
-      [[ $current_section ]] && echo >>"$filename"
-      echo "[$section_name]" >>"$filename"
+      [[ $current_section ]] && echo >>"$config_file"
+      echo "[$section_name]" >>"$config_file"
       current_section="$section_name"
     fi
     
-    echo "$key_name = $value" >>"$filename"
+    echo "$key_name = $value" >>"$config_file"
   done
 }
 
@@ -69,7 +97,7 @@ config_show() {
   done
 }
 
-## Return a newline delimited, sorted list of keys
+## Get a newline delimited, sorted list of keys
 config_keys() {
   local keys=("${!config[@]}")
   for a in "${keys[@]}"; do echo "$a"; done | sort
