863bf9a5dd88baf03c154d2663e3447299a95403
Danny Ben Shitrit <db@dannyben.com>
2024-02-15 17:27:46 +0000

    - Upgrade `approvals.bash` to 0.4.2

--- lib/bashly/libraries/test/approvals.bash
+++ lib/bashly/libraries/test/approvals.bash
@@ -1,4 +1,4 @@
-# approvals.bash v0.4.1
+# approvals.bash v0.4.2
 #
 # Interactive approval testing for Bash.
 # https://github.com/DannyBen/approvals.bash
@@ -9,7 +9,7 @@ approve() {
   cmd=$1
   last_exit_code=0
   actual=$(eval "$cmd" 2>&1) || last_exit_code=$?
-  if [[ "$allow_diff_regex" ]]; then
+  if [[ -v allow_diff_regex && "$allow_diff_regex" ]]; then
     actual=$(echo "$actual" | sed -E "s/$allow_diff_regex/*/g")
     unset allow_diff_regex
   fi
