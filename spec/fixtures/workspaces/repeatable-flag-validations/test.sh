71813d72492b9aa8d6c9dc7960e0e1b6282afd7a
Danny Ben Shitrit <db@dannyben.com>
2024-02-15 07:31:19 +0000

    fix validations for repeatable args and flags

---
+++ spec/fixtures/workspaces/repeatable-flag-validations/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+rm -f ./cli
+rm -f ./src/initialize.sh
+rm -f ./src/root_command.sh
+
+set -x
+
+bundle exec bashly generate
+
+./cli
+./cli --number 1
+./cli --number 1.1
+./cli --number a
+./cli --number 1 --number 2
+./cli --number 1.1 --number 2
+./cli --number 1 --number 2.2
+./cli --number 1 --number a
