ef1b8d85aa6f8edb72445f4c35e2882e626db34f
Danny Ben Shitrit <db@dannyben.com>
2024-02-29 05:17:26 +0000

    tuck ComposeRefinements under the Bashly namespace

--- lib/bashly/refinements/compose_refinements.rb
+++ lib/bashly/refinements/compose_refinements.rb
@@ -1,41 +1,43 @@
-module ComposeRefinements
-  refine Hash do
-    def compose(keyword = 'import')
-      result = {}
-      each do |k, v|
-        if k.to_s == keyword
-          sub = safe_load_yaml(v).compose keyword
-          if sub.is_a? Array
-            result = sub
+module Bashly
+  module ComposeRefinements
+    refine Hash do
+      def compose(keyword = 'import')
+        result = {}
+        each do |k, v|
+          if k.to_s == keyword
+            sub = safe_load_yaml(v).compose keyword
+            if sub.is_a? Array
+              result = sub
+            else
+              result.merge! sub
+            end
+          elsif v.respond_to? :compose
+            result[k] = v.compose keyword
           else
-            result.merge! sub
+            result[k] = v
           end
-        elsif v.respond_to? :compose
-          result[k] = v.compose keyword
-        else
-          result[k] = v
         end
+        result
       end
-      result
+
+      def safe_load_yaml(path)
+        loaded = YAML.load_erb_file path
+        return loaded if loaded.is_a?(Array) || loaded.is_a?(Hash)
+
+        raise Bashly::ConfigurationError, "Cannot find a valid YAML in g`#{path}`"
+      rescue Errno::ENOENT
+        raise Bashly::ConfigurationError, "Cannot find import file g`#{path}`"
+      end
     end
 
-    def safe_load_yaml(path)
-      loaded = YAML.load_erb_file path
-      return loaded if loaded.is_a?(Array) || loaded.is_a?(Hash)
-
-      raise Bashly::ConfigurationError, "Cannot find a valid YAML in g`#{path}`"
-    rescue Errno::ENOENT
-      raise Bashly::ConfigurationError, "Cannot find import file g`#{path}`"
-    end
-  end
-
-  refine Array do
-    def compose(keyword = 'import')
-      map do |x|
-        if x.respond_to? :compose
-          x.compose keyword
-        else
-          x
+    refine Array do
+      def compose(keyword = 'import')
+        map do |x|
+          if x.respond_to? :compose
+            x.compose keyword
+          else
+            x
+          end
         end
       end
     end
