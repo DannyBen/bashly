c200223a707526fd4c95c1449e3362678d5311d0
Danny Ben Shitrit <db@dannyben.com>
2024-03-12 06:00:06 +0000

    - Improve handling of unique repeatable args and flag args

--- spec/fixtures/workspaces/repeatable-escaping/test.sh
+++ spec/fixtures/workspaces/repeatable-escaping/test.sh
@@ -7,11 +7,9 @@ set -x
 bundle exec bashly generate
 
 ./cli
-./cli one "two and a half" three
-./cli one 'the phrase "hello world"' three
-./cli one 'bad"quote' three
-./cli one 'only once' 'only once'
-./cli -s one -s "two and a half" -s three
-./cli -s one -s 'the phrase "hello world"' -s three
-./cli -s one -s 'bad"quote' -s three
-./cli -s one -s 'only once' -s 'only once'
+./cli before "x x" "x x" after
+./cli before 'x "x x"' 'x "x x"' after
+./cli before 'x"x' 'x"x' after
+./cli -s before -s "x x" -s "x x" -s after
+./cli -s before -s 'x "x x"' -s 'x "x x"' -s after
+./cli -s before -s 'x"x' -s 'x"x' -s after
