2c175f9cf57a60df8c588941a3ff3026d49bbf7b
Danny Ben Shitrit <db@dannyben.com>
2024-07-14 11:42:08 +0000

    fixes

--- spec/fixtures/workspaces/repeatable-escaping/src/root_command.sh
+++ spec/fixtures/workspaces/repeatable-escaping/src/root_command.sh
@@ -1,14 +1,14 @@
 inspect_args
 
 if [[ -v args[term] ]]; then
-  read -a terms <<< "${args['term']}"
+  read -r -a terms <<<"${args['term']}"
   for t in "${terms[@]}"; do
     echo "[TERM] --> ${t}"
   done
 fi
 
 if [[ -v args[--search] ]]; then
-  read -a terms <<< "${args['--search']}"
+  read -r -a terms <<<"${args['--search']}"
   for t in "${terms[@]}"; do
     echo "[--serach] --> ${t}"
   done
