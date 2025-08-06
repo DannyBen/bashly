78bd62dd0a7bb810a647ec9dc50ecf88cefedf63
Danny Ben Shitrit <db@dannyben.com>
2025-08-06 08:26:48 +0000

    update before hook documentation comment

--- lib/bashly/libraries/hooks/before.sh
+++ lib/bashly/libraries/hooks/before.sh
@@ -3,6 +3,9 @@
 ## Any code here will be placed inside a `before_hook()` function and called
 ## before running any command (but after processing its arguments).
 ##
+## - The processed args are available to you here as `$args` and `$extra_args`
+## - The raw input array is also available in read-only mode as `$input`
+##
 ## You can safely delete this file if you do not need it.
 echo "==[ Before Hook Called ]=="
 inspect_args
